3333c341ad473466d08403a28753ccea947e8428
Rzhe <jojo920912@gmail.com>
2025-12-25 17:52:13 +0800

    homework figma

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const inputAccount = document.querySelector('.account')
-const inputPassword = document.querySelector('.password')
+const inputAccount = document.querySelector('#username')
+const inputPassword = document.querySelector('#password')
 const submitButton = document.getElementById('goLogin')
 const forgetButton = document.getElementById('forgetPassword')
 
 function handleSubmit(){
-  const account = inputAccount.value
+  const user = inputAccount.value
   const password = inputPassword.value
-  alert(`帳號資訊是:${account} 密碼資訊是${password}`)
+  alert(`帳號資訊是:${user} 密碼資訊是: ${password}`)
 }
 
 submitButton.addEventListener('click', handleSubmit)
